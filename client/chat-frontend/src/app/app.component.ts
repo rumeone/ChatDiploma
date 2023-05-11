878538513d2828735e6f5c9c69bdede302925635
rumeone <yuaroslav_29@mail.ru>
2023-05-11 17:58:45 +0700

    initial angular project

---
+++ client/chat-frontend/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {Test, TestService} from "./services/test-service/test.service";
+import {Observable} from "rxjs";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'chat-frontend';
+  testValue: Observable<Test> = this.service.getTest();
+
+  constructor(private service: TestService) {
+
+  }
+}
