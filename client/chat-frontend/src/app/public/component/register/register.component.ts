068e16d4134ba8d2ecb5658de2afd778ecc60618
rumeone <yuaroslav_29@mail.ru>
2023-05-12 18:54:07 +0700

    have been started work with basic register form

--- client/chat-frontend/src/app/public/component/register/register.component.ts
+++ client/chat-frontend/src/app/public/component/register/register.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Form, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+
+  form: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    username: new FormControl(null, [Validators.required]),
+    password: new FormControl(null, [Validators.required]),
+    passwordConfirm: new FormControl(null, [Validators.required])
+  })
+  ngOnInit(): void {
+  }
+
+  register() {
+
+  }
+  get email(): FormControl {
+    return this.form.get('email') as FormControl;
+  }
+  get username(): FormControl {
+    return this.form.get('username') as FormControl;
+  }
+  get password(): FormControl {
+    return this.form.get('password') as FormControl;
+  }
+
+  get passwordConfirm(): FormControl {
+    return this.form.get('passwordConfirm') as FormControl;
+  }
+
 
 }
--- client/chat-frontend/src/app/public/public.module.ts
+++ client/chat-frontend/src/app/public/public.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { PublicRoutingModule } from './public-routing.module';
-import { LoginComponent } from './component/login/login.component';
-import { RegisterComponent } from './component/register/register.component';
-
+import {PublicRoutingModule} from './public-routing.module';
+import {LoginComponent} from './component/login/login.component';
+import {RegisterComponent} from './component/register/register.component';
+import {MatCardModule} from '@angular/material/card';
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -13,7 +17,13 @@ import { RegisterComponent } from './component/register/register.component';
   ],
   imports: [
     CommonModule,
-    PublicRoutingModule
+    PublicRoutingModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ]
 })
-export class PublicModule { }
+export class PublicModule {
+}
