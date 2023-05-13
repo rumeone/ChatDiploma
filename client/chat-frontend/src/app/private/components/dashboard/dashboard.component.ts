b4985d32b99d96eb56c348fef44d460814d2ede9
rumeone <yuaroslav_29@mail.ru>
2023-05-13 14:13:25 +0700

    basic configure and test for socket.io and added proxy our localhost for socket.io

--- client/chat-frontend/src/app/private/components/dashboard/dashboard.component.ts
+++ client/chat-frontend/src/app/private/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ChatService} from "../../services/chat-service/chat.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+
+  title = this.chatService.getMessage();
+
+  constructor(private chatService: ChatService) {}
+
 
 }
