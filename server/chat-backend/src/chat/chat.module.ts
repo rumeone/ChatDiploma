62cecbce4bbc93d4244c28d1086abcde38ab3a71
rumeone <yuaroslav_29@mail.ru>
2023-05-13 20:54:50 +0700

    added handleConnection method

--- server/chat-backend/src/chat/chat.module.ts
+++ server/chat-backend/src/chat/chat.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChatGateway } from './gateway/chat.gateway';
+import {AuthModule} from "../auth/auth.module";
+import {UserModule} from "../user/user.module";
 
 @Module({
+  imports: [AuthModule, UserModule],
   providers: [ChatGateway]
 })
 export class ChatModule {}
--- server/chat-backend/src/chat/gateway/chat.gateway.ts
+++ server/chat-backend/src/chat/gateway/chat.gateway.ts
@@ -5,24 +5,48 @@ import {
     WebSocketGateway,
     WebSocketServer
 } from '@nestjs/websockets';
+import {AuthService} from "../../auth/service/auth.service";
+import {Server, Socket} from "socket.io";
+import {UserI} from "../../user/models/user.interface";
+import {UserService} from "../../user/service/user-service/user.service";
+import {UnauthorizedException} from "@nestjs/common";
 
 @WebSocketGateway({cors: {origin: ['https://hoppscotch.io', 'http://localhost:3000', 'http://localhost:4200']}})
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
     @WebSocketServer()
-    server;
+    server: Server;
+    title: string[] = [];
+
+    constructor(private authService: AuthService, private userService: UserService) {
+    }
 
     /*@SubscribeMessage('message')
     handleMessage(client: any, payload: any) {
         this.server.emit('message', 'test');
     }*/
 
-    handleConnection(client: any, ...args): any {
-        console.log('On connect: ');
-        this.server.emit('message', 'test');
+    async handleConnection(socket: Socket) {
+        try {
+            const decodedToken = await this.authService.verifyJwt(socket.handshake.headers.authorization);
+            const user: UserI = await this.userService.getOne(decodedToken.user.id);
+            if (!user) {
+                return this.disconnect(socket);
+            } else {
+                this.title.push('Value ' + Math.random().toString());
+                this.server.emit('message', this.title);
+            }
+        } catch (e) {
+            return this.disconnect(socket);
+        }
     }
 
-    handleDisconnect(client: any): any {
-        console.log('On disconnect: ');
+    handleDisconnect(socket: Socket): any {
+        socket.disconnect();
+    }
+
+    private disconnect(socket: Socket) {
+        socket.emit('Error ', new UnauthorizedException());
+        socket.disconnect();
     }
 }
