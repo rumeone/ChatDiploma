3d382bd4810a3529d08acdda382bab48b540080d
rumeone <yuaroslav_29@mail.ru>
2023-05-13 14:12:06 +0700

    added ongatewayconnect and disconnect

--- server/chat-backend/src/chat/gateway/chat.gateway.ts
+++ server/chat-backend/src/chat/gateway/chat.gateway.ts
@@ -1,9 +1,28 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+    OnGatewayConnection,
+    OnGatewayDisconnect,
+    SubscribeMessage,
+    WebSocketGateway,
+    WebSocketServer
+} from '@nestjs/websockets';
 
-@WebSocketGateway({cors: {origin: 'https://hoppscotch.io'}})
-export class ChatGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
-  }
+@WebSocketGateway({cors: {origin: ['https://hoppscotch.io', 'http://localhost:3000', 'http://localhost:4200']}})
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+
+    @WebSocketServer()
+    server;
+
+    /*@SubscribeMessage('message')
+    handleMessage(client: any, payload: any) {
+        this.server.emit('message', 'test');
+    }*/
+
+    handleConnection(client: any, ...args): any {
+        console.log('On connect: ');
+        this.server.emit('message', 'test');
+    }
+
+    handleDisconnect(client: any): any {
+        console.log('On disconnect: ');
+    }
 }
