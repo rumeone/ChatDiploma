336e5e3aaf30556245a783ef6d583518371f30f9
rumeone <yuaroslav_29@mail.ru>
2023-05-13 13:44:50 +0700

    installed ng socket-io and cofigured

--- client/chat-frontend/src/app/app.module.ts
+++ client/chat-frontend/src/app/app.module.ts
@@ -7,10 +7,13 @@ import {HttpClientModule} from "@angular/common/http";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {JwtModule} from "@auth0/angular-jwt";
+import {SocketIoConfig, SocketIoModule} from "ngx-socket-io";
 
 export function tokenGetter() {
   return localStorage.getItem("nestjs_chat_app");
 }
+
+const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
 
 @NgModule({
   declarations: [
@@ -27,7 +30,8 @@ export function tokenGetter() {
         tokenGetter: tokenGetter,
         allowedDomains: ['localhost:3000']
       }
-    })
+    }),
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
