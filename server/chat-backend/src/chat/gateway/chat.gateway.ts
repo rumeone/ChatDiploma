52bc9d22dbc70c68e7e37627413316a18eb48ef8
rumeone <yuaroslav_29@mail.ru>
2023-06-05 01:56:50 +0700

    added event addMessage, leaveRoom, joinRoom

--- server/chat-backend/src/chat/gateway/chat.gateway.ts
+++ server/chat-backend/src/chat/gateway/chat.gateway.ts
@@ -1,16 +1,27 @@
-import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { AuthService } from 'src/auth/service/auth.service';
-import { Socket, Server } from 'socket.io';
-import { UserI } from 'src/user/models/user.interface';
-import { UserService } from 'src/user/service/user-service/user.service';
+import {
+    OnGatewayConnection,
+    OnGatewayDisconnect,
+    SubscribeMessage,
+    WebSocketGateway,
+    WebSocketServer
+} from '@nestjs/websockets';
+import {AuthService} from 'src/auth/service/auth.service';
+import {Socket, Server} from 'socket.io';
+import {UserI} from 'src/user/models/user.interface';
+import {UserService} from 'src/user/service/user-service/user.service';
 import {OnModuleInit, UnauthorizedException} from '@nestjs/common';
-import { RoomService } from '../service/room-service/room.service';
-import { RoomI } from '../model/room/room.interface';
-import { PageI } from '../model/page.interface';
+import {RoomService} from '../service/room-service/room.service';
+import {RoomI} from '../model/room/room.interface';
+import {PageI} from '../model/page.interface';
 import {ConnectedUserI} from "../model/connected-user/connected-user.interface";
 import {ConnectedUserService} from "../service/connected-user/connected-user.service";
+import {JoinedRoomService} from "../service/joined-room/joined-room.service";
+import {RoomEntity} from "../model/room/room.entity";
+import {MessageService} from "../service/message/message.service";
+import {MessageI} from "../model/message/message.interface";
+import {JoinedRoomI} from "../model/joined-room/joined-room.interface";
 
-@WebSocketGateway({ cors: { origin: ['https://hoppscotch.io', 'http://localhost:3000', 'http://localhost:4200'] } })
+@WebSocketGateway({cors: {origin: ['https://hoppscotch.io', 'http://localhost:3000', 'http://localhost:4200']}})
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect, OnModuleInit {
 
     @WebSocketServer()
@@ -19,10 +30,14 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect, On
     constructor(private authService: AuthService,
                 private userService: UserService,
                 private roomService: RoomService,
-                private connectedUserService: ConnectedUserService) { }
+                private connectedUserService: ConnectedUserService,
+                private joinedRoomService: JoinedRoomService,
+                private messageService: MessageService) {
+    }
 
     async onModuleInit() {
         await this.connectedUserService.deleteAll();
+        await this.joinedRoomService.deleteAll();
     }
 
     async handleConnection(socket: Socket) {
@@ -33,10 +48,10 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect, On
                 return this.disconnect(socket);
             } else {
                 socket.data.user = user;
-                const rooms = await this.roomService.getRoomsForUser(user.id, { page: 1, limit: 10 });
+                const rooms = await this.roomService.getRoomsForUser(user.id, {page: 1, limit: 10});
                 rooms.meta.currentPage = rooms.meta.currentPage - 1;
 
-                await this.connectedUserService.create({ socketId: socket.id, user });
+                await this.connectedUserService.create({socketId: socket.id, user});
 
                 return this.server.to(socket.id).emit('rooms', rooms);
             }
@@ -61,7 +76,8 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect, On
 
         for (const user of createdRoom.users) {
             const connections: ConnectedUserI[] = await this.connectedUserService.findByUser(user);
-            const rooms = await this.roomService.getRoomsForUser(user.id, { page: 1, limit: 10 });
+            const rooms = await this.roomService.getRoomsForUser(user.id, {page: 1, limit: 10});
+            rooms.meta.currentPage = rooms.meta.currentPage - 1;
             for (const connection of connections) {
                 await this.server.to(connection.socketId).emit('rooms', rooms);
             }
@@ -71,11 +87,40 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect, On
     @SubscribeMessage('paginateRooms')
     async onPaginateRoom(socket: Socket, page: PageI) {
         //console.log(socket.data.user);
-        page.limit = page.limit > 100 ? 100 : page.limit;
-        page.page = page.page + 1;
-        const rooms = await this.roomService.getRoomsForUser(socket.data.user.id, page);
+        this.handleIncomingPageRequest(page);
+        const rooms = await this.roomService.getRoomsForUser(socket.data.user.id, this.handleIncomingPageRequest(page));
         rooms.meta.currentPage = rooms.meta.currentPage - 1;
         return this.server.to(socket.id).emit('rooms', rooms);
+    }
+
+    @SubscribeMessage('joinRoom')
+    async onJoinRoom(socket: Socket, room: RoomI) {
+        const messages = await this.messageService.findMessageForRoom(room, {limit: 10, page: 1});
+        messages.meta.currentPage = messages.meta.currentPage - 1;
+
+        await this.joinedRoomService.create({socketId: socket.id, user: socket.data.user, room});
+
+        await this.server.to(socket.id).emit('messages', messages);
+
+    }
+
+    @SubscribeMessage('leaveRoom')
+    async onLeaveRoom(socket: Socket) {
+        await this.joinedRoomService.deleteBySocketId(socket.id);
+    }
+
+    @SubscribeMessage('addMessage')
+    async onAddMessage(socket: Socket, message: MessageI) {
+        const createdMessage: MessageI = await this.messageService.create({...message, user: socket.data.user});
+        const room: RoomI = await this.roomService.getRoom(createdMessage.room.id);
+        const joinedUsers: JoinedRoomI[] = await this.joinedRoomService.findByRoom(room);
+
+    }
+
+    private handleIncomingPageRequest(page: PageI) {
+        page.limit = page.limit > 100 ? 100 : page.limit;
+        page.page = page.page + 1;
+        return page;
     }
 
 }
